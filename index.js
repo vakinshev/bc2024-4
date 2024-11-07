1b043cd0cbf32ebb20193e146190040a981330ee
Vladyslav bc2024-4 <vladyslav.akinshev@lnu.edu.ua>
2024-11-07 11:20:51 +0200

    last tested part of index.js

--- index.js
+++ index.js
@@ -1,130 +1,136 @@
-const { program } = require("commander");
 const http = require("http");
-const fsp = require("node:fs/promises");
-const path = require("node:path");
+const { program } = require("commander");
+const fs = require("fs").promises;
+const path = require("path");
 const superagent = require("superagent");
+
 function preparing() {
-    program
-        .option("-h, --host <value>", "Host location")
-        .option("-p, --port <value>", "Port location")
-        .option("-c, --cache <value>", "Cache location");
-    program.parse();
+  program
+    .requiredOption("-h, --host <host>", "Server host")
+    .requiredOption("-p, --port <port>", "Server port")
+    .requiredOption("-c, --cache <path>", "Path to cache directory")
+    .parse(process.argv);
 
-    const options = program.opts();
+  const options = program.opts();
 
-    if (!options.host || !options.port || !options.cache) {
-        throw new Error("Please, specify necessary parameters.");
-    }
+  if (!options.host || !options.port || !options.cache) {
+    throw Error("Please specify necessary parameters: host, port, and cache path");
+  }
 
-    return options;
+  return options;
 }
 
 const options = preparing();
+const getCacheFilePath = (code) => path.join(options.cache, `${code}.jpg`);
 
-function getCachedPicture(way) {
-    return fsp.readFile(way);
+async function getPicture(filePath) {
+  return fs.readFile(filePath);
 }
 
-function cachePicture(way, data) {
-    return fsp.writeFile(way, data);
+async function savePicture(filePath, data) {
+  return fs.writeFile(filePath, data);
 }
 
-function deleteCachedPicture(way) {
-    return fsp.unlink(way);
+async function deletePicture(filePath) {
+  return fs.unlink(filePath);
 }
 
-function downloadPicture(code) {
-    const url = `https://http.cat/${code}`;
-    return superagent.get(url).buffer(true);
+async function downloadPicture(code) {
+  const response = await superagent.get(`https://http.cat/${code}`);
+  return response.body;
 }
 
 function debug(req, code) {
-    console.log(`Request method: ${req.method}\tResponse code: ${code}`);
+  console.log(`Request method: ${req.method} \t Response code: ${code}`);
 }
 
-//--------------------------------------------//
-function requestListener(req, res) {
-    if (req.url === "/") {
-        res.writeHead(404);
-        res.end();
-        debug(req, 404);
-        return;
-    }
+const requestListener = async (req, res) => {
+  const urlParts = req.url.split("/");
+  const code = urlParts[1];
 
-    const code = req.url.slice(1);
-    const way = path.join(__dirname, options.cache, `${code}.jpg`);
+  if (!code) {
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end("HTTP code not specified");
+    debug(req, 400);
+    return;
+  }
 
+  const filePath = getCacheFilePath(code);
+
+  try {
     switch (req.method) {
-        case "GET":
-            getCachedPicture(way)
-                .then((result) => {
-                    res.writeHead(200, { "Content-Type": "image/jpeg" });
-                    res.end(result);
-                    debug(req, 200);
-                })
-                .catch(() => {
-                    downloadPicture(code)
-                        .then((result) => {
-                            cachePicture(way, result.body);
-                            res.writeHead(200, { "Content-Type": "image/jpeg" });
-                            res.end(result.body);
-                            debug(req, 200);
-                        })
-                        .catch(() => {
-                            res.writeHead(404);
-                            res.end();
-                            debug(req, 404);
-                        });
-                });
-            break;
+      case "GET":
+        try {
+          const data = await getPicture(filePath);
+          res.writeHead(200, { "Content-Type": "image/jpeg" });
+          res.end(data);
+          debug(req, 200);
+        } catch (error) {
+          if (error.code === "ENOENT") {
+            try {
+              const imageData = await downloadPicture(code);
+              await savePicture(filePath, imageData);
+              res.writeHead(200, { "Content-Type": "image/jpeg" });
+              res.end(imageData);
+              debug(req, 200);
+            } catch (fetchError) {
+              res.writeHead(404, { "Content-Type": "text/plain" });
+              res.end("Image not found on http.cat");
+              debug(req, 404);
+            }
+          } else {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Server error");
+            debug(req, 500);
+          }
+        }
+        break;
 
-        case "PUT":
-            let data = [];
-            req.on("data", (chunk) => {
-                data.push(chunk);
-            });
-            req.on("end", () => {
-                cachePicture(way, Buffer.concat(data))
-                    .then(() => {
-                        res.writeHead(201);
-                        res.end();
-                        debug(req, 201);
-                    })
-                    .catch(() => {
-                        res.writeHead(500);
-                        res.end("Internal Server Error");
-                    });
-            });
-            break;
+      case "PUT":
+        let body = [];
+        req.on("data", (chunk) => {
+          body.push(chunk);
+        }).on("end", async () => {
+          body = Buffer.concat(body);
+          await savePicture(filePath, body);
+          res.writeHead(201, { "Content-Type": "text/plain" });
+          res.end("Image saved");
+          debug(req, 201);
+        });
+        break;
 
-        case "DELETE":
-            deleteCachedPicture(way)
-                .then(() => {
-                    res.writeHead(200);
-                    res.end();
-                    debug(req, 200);
-                })
-                .catch(() => {
-                    res.writeHead(404);
-                    res.end();
-                    debug(req, 404);
-                });
-            break;
+      case "DELETE":
+        try {
+          await deletePicture(filePath);
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.end("Image deleted");
+          debug(req, 200);
+        } catch (error) {
+          if (error.code === "ENOENT") {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Image not found");
+            debug(req, 404);
+          } else {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Server error");
+            debug(req, 500);
+          }
+        }
+        break;
 
-        default:
-            res.writeHead(405);
-            res.end();
-            debug(req, 405);
-            break;
+      default:
+        res.writeHead(405, { "Content-Type": "text/plain" });
+        res.end("Method not allowed");
+        debug(req, 405);
     }
-}
-//--------------------------------------------------//
-function main() {
-    const server = http.createServer(requestListener);
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server error");
+    debug(req, 500);
+  }
+};
 
-    server.listen(options.port, options.host, () => {
-        console.log(`Server running at http://${options.host}:${options.port}`);
-    });
-}
-
-main();
+const server = http.createServer(requestListener);
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}/`);
+});
